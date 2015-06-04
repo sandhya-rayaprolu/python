66e5fde5a11ec517fd1d949f653d7311759d03da
Sandhya Rayaprolu <sandhya.rayaprolu@gmail.com>
2015-06-04 16:37:31 -0700

    fixes for code format issues

--- movie_list.py
+++ movie_list.py
@@ -5,61 +5,61 @@ import fresh_tomatoes
 movie_list = []
 
 # create a movie object
-wizard_of_oz =  media.Movie("The Wizard of Oz",
-                            "http://ia.media-imdb.com/images/M/"
-                            "MV5BMTU0MTA2OTIwNF5BMl5BanBnXkFtZTcwMzA0Njk3OA@@."
-                            "_V1_SY317_CR10,0,214,317_AL_.jpg",
-                            "https://www.youtube.com/watch?v=H_3T4DGw10U")
+wizard_of_oz = media.Movie(
+    "The Wizard of Oz",
+    "http://ia.media-imdb.com/images/M/MV5BMTU0MTA2OTIwNF5BMl5BanBnXkFtZTcwMz"
+    "A0Njk3OA@@._V1_SY317_CR10,0,214,317_AL_.jpg",
+    "https://www.youtube.com/watch?v=H_3T4DGw10U")
 # add movie to list
 movie_list.append(wizard_of_oz)
 
 # create a movie object
-titanic = media.Movie("Titanic",
-                      "http://ia.media-imdb.com/images/M/"
-		      "MV5BMjExNzM0NDM0N15BMl5BanBnXkFtZTcwMzkxOTUwNw@@."
-		      "_V1_SY317_CR0,0,214,317_AL_.jpg",
-                      "https://www.youtube.com/watch?v=zCy5WQ9S4c0")
+titanic = media.Movie(
+    "Titanic",
+    "http://ia.media-imdb.com/images/M/MV5BMjExNzM0NDM0N15BMl5BanBnXkFtZTcwMz"
+    "kxOTUwNw@@._V1_SY317_CR0,0,214,317_AL_.jpg",
+    "https://www.youtube.com/watch?v=zCy5WQ9S4c0")
 # add movie to list
-movie_list.append(titanic)   
+movie_list.append(titanic)
 
 # create a movie object
-matrix = media.Movie("The Matrix",
-                     "http://ia.media-imdb.com/images/M/"
-                     "MV5BMTkxNDYxOTA4M15BMl5BanBnXkFtZTgwNTk0NzQxMTE@."
-		     "_V1_SX214_AL_.jpg",
-                     "https://www.youtube.com/watch?v=m8e-FF8MsqU")
+matrix = media.Movie(
+    "The Matrix",
+    "http://ia.media-imdb.com/images/M/MV5BMTkxNDYxOTA4M15BMl5BanBnXkFtZTgwNTk"
+    "0NzQxMTE@._V1_SX214_AL_.jpg",
+    "https://www.youtube.com/watch?v=m8e-FF8MsqU")
 # add movie to list
 movie_list.append(matrix)
 
 # create a movie object
-frozen = media.Movie("Frozen",
-                     "http://resizing.flixster.com/M3JgYWjuLaOPruIuhLfHA0GwmtY=/"
-		     "180x267/dkpu1ddg7pbsk.cloudfront.net/movie/11/17/35/"
-		     "11173584_ori.jpg",
-                     "https://www.youtube.com/watch?v=TbQm5doF_Uc")
+frozen = media.Movie(
+    "Frozen",
+    "http://resizing.flixster.com/M3JgYWjuLaOPruIuhLfHA0GwmtY=/180x267/dkpu1dd"
+    "g7pbsk.cloudfront.net/movie/11/17/35/11173584_ori.jpg",
+    "https://www.youtube.com/watch?v=TbQm5doF_Uc")
 # add movie to list
 movie_list.append(frozen)
 
 # create a movie object
-imitation_game = media.Movie("The Imitation Game",
-                             "http://ia.media-imdb.com/images/M/"
-			     "MV5BNDkwNTEyMzkzNl5BMl5BanBnXkFtZTgwNTAwNzk3MjE@."
-                             "_V1_SY317_CR0,0,214,317_AL_.jpg",
-                             "https://www.youtube.com/watch?v=S5CjKEFb-sM")
+imitation_game = media.Movie(
+    "The Imitation Game",
+    "http://ia.media-imdb.com/images/M/MV5BNDkwNTEyMzkzNl5BMl5BanBnXkFtZTgwNT"
+    "AwNzk3MjE@._V1_SY317_CR0,0,214,317_AL_.jpg",
+    "https://www.youtube.com/watch?v=S5CjKEFb-sM")
 # add movie to list
 movie_list.append(imitation_game)
 
 # create a movie object
-star_wars = media.Movie("Star Wars: Episode VII - The Force Awakens",
-                        "http://resizing.flixster.com/HBIJnzHib7pKy_C1Yxau8jyYhOg=/"
-		        "180x267/dkpu1ddg7pbsk.cloudfront.net/movie/11/18/17/"
-		        "11181757_ori.jpg",
-                        "https://www.youtube.com/watch?v=OMOVFvcNfvE")
+star_wars = media.Movie(
+    "Star Wars: Episode VII - The Force Awakens",
+    "http://resizing.flixster.com/HBIJnzHib7pKy_C1Yxau8jyYhOg=/180x267/dkpu1dd"
+    "g7pbsk.cloudfront.net/movie/11/18/17/11181757_ori.jpg",
+    "https://www.youtube.com/watch?v=OMOVFvcNfvE")
 # add movie to list
 movie_list.append(star_wars)
 
-# call functions from fresh_tomotoes.py to create the content for all movies in list
+# call function from fresh_tomotoes.py to create content for all movies in list
 fresh_tomatoes.create_movie_tiles_content(movie_list)
 
-# use function from fresh_tomatoes.py to generate the HTML page to display the movie list
+# use function from fresh_tomatoes.py to generate HTML page with the movie list
 fresh_tomatoes.open_movies_page(movie_list)
